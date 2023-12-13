98fd6db8d11cee53575793ce8d06c999eab1ae8e
Ole Demay <olesyademy@gmail.com>
2023-12-13 05:56:17 +0100

    Updated, keeping highest score

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import  Screen
+from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
@@ -36,15 +36,14 @@ while game_is_on:
 
     # Detect collusion with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collusion with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
 
 
 
